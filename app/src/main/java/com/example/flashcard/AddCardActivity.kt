8761ded076032edb96ad6e11e7f0200122829779
Gassendy <renegassen42@gmail.com>
2023-08-18 13:36:37 -0700

    Lab3

--- app/src/main/java/com/example/flashcard/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcard/AddCardActivity.kt
@@ -3,6 +3,7 @@ package com.example.flashcard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
@@ -12,10 +13,13 @@ class AddCardActivity : AppCompatActivity() {
     val save = findViewById<ImageView>(R.id.download)
     val question = findViewById<EditText>(R.id.question)
     val reponse = findViewById<EditText>(R.id.rep)
+    lateinit var flashcardatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
-
+        flashcardatabase = FlashcardDatabase(this)
+        allFlashcards = flashcardatabase.getAllCards().toMutableList()
 
 
         returnToMain.setOnClickListener() {
@@ -24,34 +28,27 @@ class AddCardActivity : AppCompatActivity() {
         }
 
         save.setOnClickListener(){
-              val donnees1 = question.text.toString()
-              val donnees2 = reponse.text.toString()
-            getAndShowQuestion(donnees1,donnees2)
+            getAndShowQuestion(question.toString(),reponse.toString())
             }
 
         }
     private  fun getAndShowQuestion(data1: String, data2: String){
         val intent = Intent(this,MainActivity::class.java)
+        val quest = question.text.toString()
+        val rep = reponse.text.toString()
+        if(verifyData(quest,rep)){
+            val question_ans = Flashcard(quest,rep)
+            flashcardatabase.insertCard(question_ans)
+            allFlashcards = flashcardatabase.getAllCards().toMutableList()
+        }
+
         intent.putExtra("donnees",data1)
         intent.putExtra("donnees2",data2)
         startActivity(intent)
     }
 
-    private fun verifyData(data1: String, data2: String){
-        while(data1 =="" && data2 ==""){
-            if(data1 ==""){
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("error")
-                builder.setMessage("Vous devez remplir le champ question")
-                builder.show()
-            }
-            else if(data2 ==""){
-                val builder2 = AlertDialog.Builder(this)
-                builder2.setTitle("Error")
-                builder2.setMessage("Vous devez remplir le champ reponse")
-                builder2.show()
-            }
-        }
+    private fun verifyData(data1: String, data2: String): Boolean{
+       return !(TextUtils.isEmpty(data1) && (TextUtils.isEmpty(data2)))
     }
 
     }
--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,23 +1,61 @@
 package com.example.flashcard
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flashcardatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        flashcardatabase = FlashcardDatabase(this)
+        allFlashcards = flashcardatabase.getAllCards().toMutableList()
+
         val textView = findViewById<TextView>(R.id.flashCard_question)
         val textAnswer = findViewById<TextView>(R.id.answer)
         val ajout = findViewById<ImageView>(R.id.addView)
+        val AdvanceQuestion = findViewById<ImageView>(R.id.next)
+        val DeleteAnswerQuestion = findViewById<ImageView>(R.id.deleteQuestion)
+
+        AdvanceQuestion.setOnClickListener(){
+            val ListQuestion = flashcardatabase.getAllCards()
+            if(ListQuestion.isNotEmpty()){
+                val randomIndex = Random.nextInt(0,ListQuestion.size)
+                val randomQuestion = ListQuestion[randomIndex]
+                textView.text = randomQuestion.question
+                textAnswer.text = randomQuestion.answer
+            }
+        }
+
+        DeleteAnswerQuestion.setOnClickListener(){
+            var index: Int = 0
+            allFlashcards = flashcardatabase.getAllCards().toMutableList()
+            for(index in 0.. allFlashcards.size) {
+                val questionToDelete = textView.text.toString()
+                if (allFlashcards.isNotEmpty()) {
+
+                    flashcardatabase.deleteCard(questionToDelete)
+                    textView.text = ""
+                    textAnswer.text = ""
+
+                }
+            }
+            allFlashcards[index--]
+        }
+
+        if(allFlashcards.size >0){
+            var (question, answer) = allFlashcards[0]
+            textView.text = question
+            textAnswer.text = answer
+        }
 
         textView.setOnClickListener() {
             if (textAnswer.visibility == View.GONE) {
@@ -35,14 +73,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(changeToAdd)
         }
 
-        val receivedData = intent.getStringExtra("data")
-        val receivedData2 = intent.getStringExtra(("data2"))
-        if(receivedData != null && receivedData2 != null){
-            textView.text = receivedData
-            Toast.makeText(this,receivedData, Toast.LENGTH_SHORT).show()
-            textAnswer.text = receivedData2
-                Toast.makeText(this,receivedData2, Toast.LENGTH_SHORT).show()
+        val receiveData = intent.getStringExtra("data")
+        val receiveData2 = intent.getStringExtra(("data2"))
+        if(receiveData != null && receiveData2 != null){
+            flashcardatabase.insertCard(Flashcard(receiveData.toString(),receiveData2.toString()))
+            allFlashcards = flashcardatabase.getAllCards().toMutableList()
+            textView.text = receiveData
+            textAnswer.text = receiveData2
         }
+
 
 
     }
